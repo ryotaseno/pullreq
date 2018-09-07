a3559b7f144402fe164bca14e309fc769d3ba6c8
reo <reo0513freedom@gmail.com>
2018-09-07 14:09:30 +0900

    reo maked monsters take1

--- Yatagarasu.java
+++ Yatagarasu.java
@@ -2,12 +2,23 @@ package takahara;
 
 public class Yatagarasu extends Monster implements Bird {
 
+	public Yatagarasu() {
+		Life life = new Life();
+		life.setHp(2000);
+	}
+
 	public void run(){
 		System.out.println("八咫烏が走っています。");
 	}
 
-	public void attack() {
-		System.out.println("八咫烏がつついています。");
+	public void attack(Monster monster) {
+		System.out.println("八咫烏が噛みついています。");
+		monster.setHp(monster.getHp() - 1500);
+		if(monster.getHp() <= 0) {
+			life.dead();
+		}else {
+			system.out.println(this.name + "の残りHPは" + monster.getHp() + "です。");
+		}
 	}
 
 }
---
+++ Zombie.java
@@ -0,0 +1,27 @@
+package takahara;
+
+public class Zombie extends Monster implements Human {
+
+	String name;
+
+	public Yatagarasu(String name) {
+		this.name = name;
+		Life life = new Life;
+		life.setHp(9999);
+	}
+
+	public void run(){
+		System.out.println("ゾンビが走っています。");
+	}
+
+	public void attack(Monster monster) {
+		System.out.println("ゾンビが噛みついています。");
+		monster.setHp(monster.getHp() - 800);
+		if(monster.getHp() <= 0) {
+			life.dead();
+		}else {
+			system.out.println(this.name + "の残りHPは" + monster.getHp() + "です。");
+		}
+	}
+
+}
