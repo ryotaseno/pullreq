59707e4f63028fefe536ccf69a2155e423ef537c
RyotaSeno <seno.oic@gmail.com>
2018-09-11 11:15:33 +0900

    add Character

---
+++ takahara/Character.java
@@ -0,0 +1,35 @@
+package takahara;
+
+public abstract class Character extends Life {
+
+	private int lv;
+	private String name;
+	private Weapon wp;	
+
+	abstract void attack();
+	
+	public int getLv() {
+		return lv;
+	}
+	
+	public void setLv(int lv) {
+		this.lv = lv;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(string name) {
+		this.name = name;
+	}
+	
+	public Weapon getWp() {
+		return wp;
+	}
+	
+	public void setWeapon(Weapon wp) {
+		this.wp = wp;
+	}
+
+}
