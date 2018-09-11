d8f800450f3945f2fd2a0bff296d2c0104efc460
RyotaSeno <seno.oic@gmail.com>
2018-09-11 10:18:04 +0900

    conflicts

--- takahara/Yatagarasu.java
+++ takahara/Yatagarasu.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD:takahara/Yatagarasu.java
+
 package takahara;
 
 public class Yatagarasu extends Monster implements Bird {
@@ -26,32 +26,3 @@ public class Yatagarasu extends Monster implements Bird {
 
 
 }
-=======
-package takahara;
-
-public class Yatagarasu extends Monster implements Bird {
-
-	String name;
-
-	public Yatagarasu(String name) {
-		super.setName(name);
-		super.setHp(2000);
-	}
-
-	public void run(){
-		System.out.println("八咫烏が走っています。");
-	}
-
-	public void attack(Monster monster) {
-		System.out.println("八咫烏が噛みついています。");
-		monster.setHp(monster.getHp() - 1500);
-		if(monster.getHp() <= 0) {
-			super.dead(monster.getName());
-		}else {
-			System.out.println(monster.getName() + "の残りHPは" + monster.getHp() + "です。");
-		}
-	}
-
-
-}
->>>>>>> seno:takahara/Yatagarasu.java
