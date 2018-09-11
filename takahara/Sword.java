83c4e1b570eddde46b9a5b43e1ae44be277eb208
seki <sekiyuki0203@gmail.com>
2018-09-11 09:32:06 +0900

    kannsei

---
+++ takahara/Sword.java
@@ -0,0 +1,14 @@
+package takahara;
+
+public class Sword extends Weapon{
+	public Sword(String name, int power) {
+		super(name, power);
+	}
+
+	public int attack(int hp) {
+		hp -= this.power ;
+		System.out.println("d—¢€–S");
+		return hp;
+	}
+
+}
