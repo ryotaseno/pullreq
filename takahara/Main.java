9afe755ea73c83ab99664e4d3ea14241208d3cc8
reo <reo0513freedom@gmail.com>
2018-09-11 10:56:53 +0900

    main 3

--- takahara/Main.java
+++ takahara/Main.java
@@ -9,11 +9,41 @@ public class Main {
 		Yatagarasu yatagarasuB = new Yatagarasu("八咫烏B");
 		Zombie zombieA = new Zombie("ゾンビA");
 
-		//aaaaaaa
+		Hero huji = new Hero("藤井先生");
+		Wizard ito = new Wizard("伊藤先生");
 
-		zombieA.attack(yatagarasuB);
-		zombieA.attack(yatagarasuB);
-		zombieA.attack(yatagarasuB);
+		huji.attack(yatagarasuB);
+		//hujiが剣を装備する処理を書く
+		Sword sword = new Sword();
+		huji.setWeapon(sword);
+		if(huji.getWp == null) {
+			//武器を装備できた場合
+			System.out.println("藤井先生は武器を装備できませんでした");
+		}else {
+			//武器を装備できなかった場合
+			System.out.println("藤井先生は武器を装備できました。");
+		}
+		//伊藤先生が死ぬまで攻撃する処理を書く
+		while(true) {
+			if(huji.getWp == null) {
+				//素手で殴る
+				huji.attack(yatagarasuB);
+			}else {
+				//剣で殴る
+				huji.attack(yatagarasuB.getHp());
+			}
+
+			//ゾンビが伊藤先生を殴る
+			zombieA.attack(ito);
+			if(ito.getHp <= 0) {
+				System.out.println("伊藤先生が死にました。");
+				break;
+			}
+		}
+
+		//zombieA.attack(yatagarasuB);
+		//zombieA.attack(yatagarasuB);
+		//zombieA.attack(yatagarasuB);
 	}
 
 }
