d42d079e8253c2a40781c6d5ff91f724b4e7d0cb
daphnia <daphnia.kobayashi@gmail.com>
2018-09-13 09:53:55 +0900

    created Hero.java

--- takahara/Hero.java
+++ takahara/Hero.java
@@ -2,24 +2,39 @@ package takahara;
 
 public class Hero extends Character implements Human{
 
-	public void attack(Monster monster) {
+	public boolean attack(Monster monster) {
 
-		System.out.println("ヒーローは素手で攻撃を行った");
-
-		monster.setHp(monster.getHp() - 800);
-		if(monster.getHp() <= 0) {
-			super.dead(monster.getName());
-		}else {
-			System.out.println(monster.getName() + "の残りHPは" + monster.getHp() + "です。");
+		//武器を持っているかの判定
+		if( super.getWp() != null ){
+			//持っている武器のattack()を呼び出す
+			monster.setHp(super.getWp().attack(monster.getHp()));
+			//生存判定
+			boolean deadFlag = super.checkDead(monster);
+			return deadFlag;
+		} else {
+			//素手で殴る場合の処理
+			System.out.println(super.getName() + "が素手で攻撃しています");
+			this.setHp(this.getHp() - 1500);
+			//生存判定
+			boolean deadFlag = super.checkDead(monster);
+			return deadFlag;
 		}
+
 	}
 
 	public void run(){
 		System.out.println("勇者は走っています");
 	}
-
+	//コンストラクタ
 	public Hero(String name) {
 		super.setName(name);
+	}
+
+	//コンストラクタ
+	public Hero() {
+		super.setName("ななしさん");
+		super.setHp(100);
+		super.setLv(1);
 	}
 
 	@Override
